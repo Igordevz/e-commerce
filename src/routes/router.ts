bf9f26cc54a40b66d73fb40e597e0f30bab6b543
Igordevz <programadorigor.dev@gmail.com>
2023-04-23 21:11:32 -0300

    config initial page api

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,6 +4,6 @@ import CreateUser from "../controllers/create_user";
 export const router = Router();
 
 router.get('/', (req, res) =>{
-   return res.status(200).send('ola mundo');
+   return res.status(200).send('welcome the Api');
 })
 router.post('/create_user', CreateUser)
