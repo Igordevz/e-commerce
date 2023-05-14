8cb15aa2dc442b9af1c719f209ada2608184ad3c
Igordevz <programadorigor.dev@gmail.com>
2023-05-14 20:51:44 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,16 @@
 import express from "express";
 import DbConect from "./database/mongodb";
-import * as dotenv  from "dotenv";
+import * as dotenv from "dotenv";
 import { router } from "./routes/router";
-dotenv.config()
+dotenv.config();
 
 async function Bootstrap() {
-  
   const app = express();
   app.use(express.json());
-  
+
   await DbConect();
-  app.use(router)
-
+  app.use(router);
   const Door = 3000;
-
   app.listen(Door, () => {
     console.log(`HTTP server running ${Door}`);
   });
